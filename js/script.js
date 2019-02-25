a70a6ee4c8821d6cc3c90df0a16dade37bd1be13
pashik913 <45880699+pashik913@users.noreply.github.com>
2019-02-25 22:43:33 +0300

    правки карты, позиционирования формы и состояний инпутов

--- js/script.js
+++ js/script.js
@@ -57,27 +57,3 @@
       }
     }
   });
-
-  var mapLink = document.querySelector(".contacts-button-map");
-
-  var mapPopup = document.querySelector(".contacts-map");
-  var mapClose = mapPopup.querySelector(".modal-close");
-
-  mapLink.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.add("modal-show");
-  });
-
-  mapClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
-  });
-
-  window.addEventListener("keydown", function (evt) {
-    evt.preventDefault();
-    if (evt.keyCode === 27) {
-      if (mapPopup.classList.contains("modal-show")) {
-        mapPopup.classList.remove("modal-show");
-      }
-    }
-  });
